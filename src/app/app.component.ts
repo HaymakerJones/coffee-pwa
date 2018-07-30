5a2a700fbd0626bb47e53106fcf45032dde7fcd2
HaymakerJones <js388111@gmail.com>
2018-07-30 15:30:56 -0400

    ready for service worker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  constructor(private snackbar: MatSnackBar) { }
+
+  ngOnInit() {
+    if (!navigator['standalone']) {
+      //this means we are on ios and in the browser
+      this.snackbar.open("You can add this PWA to your homescreen", null, { duration: 3000 });
+    }
+
+    if (navigator['standalone'] == undefined) {
+      //Its not ios
+      if (window.matchMedia("(display-mode: browser").matches) {
+        //We are in the browser
+        window.addEventListener("beforeinstallprompt", event => {
+          event.preventDefault();
+          const sb = this.snackbar.open("Do you want to install this app?", "Install", { duration: 5000 });
+          sb.onAction().subscribe(
+            () => {
+              (event as any).prompt();
+              (event as any).userChoice.then(result => {
+                if (result.outcome == "dismissed") {
+                  //TODO: Track no installation
+                }
+                else {
+                  //TODO: it was installed
+                }
+              })
+            }
+          );
+        })
+      }
+    }
+
+  }
 }
